0cde2783619cf72b8303a8ef0b106558d3874f0d
sanc <hlsandunichamika@gmail.com>
2020-11-28 11:12:07 +0530

    Initial Commit

---
+++ iSpeak/iSpeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSpeakApp.swift
+//  iSpeak
+//
+//  Created by Sanduni Chamika on 2020-11-28.
+//
+
+import SwiftUI
+
+@main
+struct iSpeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
